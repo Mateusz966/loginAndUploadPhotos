05330fea00cc7e2864f84926957f1ded9bad17ba
Mateusz Szuber <mati@MacBook-Air-Mateusz-2.local>
2019-08-24 19:14:50 +0200

    creating posts model

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as mongoose from 'mongoose';
+import 'dotenv/config';
  
 class App {
   public app: express.Application;
@@ -9,6 +11,7 @@ class App {
     this.app = express();
     this.port = port;
  
+    this.connectToDatabase();
     this.initializeMiddlewares();
     this.initializeControllers(controllers);
   }
@@ -27,6 +30,17 @@ class App {
     this.app.listen(this.port, () => {
       console.log(`App listening on the port ${this.port}`);
     });
+  }
+
+  private connectToDatabase() {
+    const {
+      MONGO_USER,
+      MONGO_PASSWORD,
+      MONGO_PATH,
+    } = process.env;
+    console.log(MONGO_USER, MONGO_PASSWORD, MONGO_PATH);
+    
+    mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`, {useNewUrlParser: true});
   }
 }
  
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,6 @@
 import App from './app';
 import PostsController from './posts/posts.controller';
-import * as mongoose from 'mongoose';
-import 'dotenv/config';
- 
-const {
-  MONGO_USER,
-  MONGO_PASSWORD,
-  MONGO_PATH,
-} = process.env;
- 
-mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`);
+
 const app = new App(
   [
   new PostsController(),
