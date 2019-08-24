5167c7e535c4ea0bdcdd42e62afc0503c07a7f8f
Mateusz Szuber <mati@MacBook-Air-Mateusz-2.local>
2019-08-24 18:48:14 +0200

    init repo, create server, create posts interface and controller, connect with mongodb

---
+++ src/posts/post.interface.ts
@@ -0,0 +1,8 @@
+interface Post {
+  author: string,
+  content: string,
+  imageUrl: string,
+  title: string,
+}
+
+export default Post;
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import App from './app';
+import PostsController from './posts/posts.controller';
+import * as mongoose from 'mongoose';
+import 'dotenv/config';
+ 
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_PATH,
+} = process.env;
+ 
+mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`);
+const app = new App(
+  [
+  new PostsController(),
+  ],
+  5000,
+);
+
+app.listen();
